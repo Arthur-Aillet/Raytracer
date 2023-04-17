c1cdf8700da36e04d3045adc8ac645fd4acb7568
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-04-17 14:48:04 +0200

    feat(ppm interface): can now select the name of the ppm at the creation of the object (example in the main)

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@
 
 mod ppm_interface;
 
+use std::env;
+
 fn main() -> std::io::Result<()> {
-    let mut ppm = ppm_interface::PPMInterface::new(String::from("test.ppm"));
+    let args: Vec<String> = env::args().collect();
+    let mut ppm = ppm_interface::PPMInterface::new(String::from(args[1].clone()));
+
     ppm.write(1000, 1000);
     Ok(())
 }
