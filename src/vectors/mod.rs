672a95769c6abc7c52264ba69dc7edf8f31cf3d8
Aillet <arthur.aillet@epitech.eu>
2023-04-18 01:27:26 +0200

    feat: vector addition implementation

--- src/vectors/mod.rs
+++ src/vectors/mod.rs
@@ -5,15 +5,62 @@
 // vector
 //
 
+use matrix::Matrix;
+use crate::matrix;
+use std::ops::Add;
+
+#[derive(Debug, Clone)]
 pub struct Point {
     pub x: f64,
     pub y: f64,
     pub z: f64,
 }
 
+#[derive(Debug)]
 pub struct VectorF {
     pub origin : Point,
     pub direction: Point,
+}
+
+impl Add<VectorF> for VectorF {
+    type Output = VectorF;
+    fn add(self, other: VectorF) -> VectorF {
+        VectorF {
+            origin: Point {
+                x: self.origin.x + other.origin.x,
+                y: self.origin.y + other.origin.y,
+                z: self.origin.z + other.origin.z,
+            },
+            direction: Point {
+                x: self.direction.x + other.direction.x,
+                y: self.direction.y + other.direction.y,
+                z: self.direction.z + other.direction.z,
+            },
+        }
+    }
+}
+
+impl VectorF {
+    pub fn rotate(&mut self, x : f64, y : f64, z : f64) {
+        let mut rotated = Matrix::new(3, 1);
+
+        rotated.multiply(&Matrix::euler_rotation(x, y, z));
+        self.direction.x = rotated.data[0][0];
+        self.direction.y = rotated.data[0][1];
+        self.direction.z = rotated.data[0][2];
+    }
+    pub fn add(mut self, other: VectorF) {
+        self.origin = Point {
+            x: self.origin.x + other.origin.x,
+            y: self.origin.y + other.origin.y,
+            z: self.origin.z + other.origin.z,
+        };
+        self.direction = Point {
+            x: self.direction.x + other.direction.x,
+            y: self.direction.y + other.direction.y,
+            z: self.direction.z + other.direction.z,
+        }
+    }
 }
 
 pub fn number_of_solution(a: f64, b: f64, c: f64) -> i8 {
