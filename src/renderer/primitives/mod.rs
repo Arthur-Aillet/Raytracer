c94fccb878c0b6ade125017e10e250965435ab2c
Aillet <arthur.aillet@epitech.eu>
2023-04-19 02:20:57 +0200

    style: better imports in primitives

--- src/renderer/primitives/mod.rs
+++ src/renderer/primitives/mod.rs
@@ -6,25 +6,27 @@
 //
 
 use crate::vectors;
-use crate::vectors::resolve_quadratic_equation;
+use vectors::VectorF;
+use vectors::Point;
+use vectors::resolve_quadratic_equation;
 
 pub trait Object {
-    fn intersection(&self, ray: vectors::VectorF) -> bool;
+    fn intersection(&self, ray: VectorF) -> bool;
 }
 
 #[derive(Debug)]
 pub struct Sphere {
-    pub origin: vectors::Point,
+    pub origin: Point,
     pub radius: f64,
 }
 
 pub struct Plan {
-    origin: vectors::Point,
-    endPoint: vectors::Point,
+    origin: Point,
+    endPoint: Point,
 }
 
 impl Sphere {
-    pub fn set_origin(&mut self, origin: vectors::Point) {
+    pub fn set_origin(&mut self, origin: Point) {
         self.origin = origin;
     }
     pub fn set_radius(&mut self, radius: f64) {
@@ -33,16 +35,16 @@ impl Sphere {
 }
 
 impl Plan {
-    pub fn set_origin(&mut self, point: vectors::Point) {
+    pub fn set_origin(&mut self, point: Point) {
         self.origin = point;
     }
-    pub fn set_endPoint(&mut self, point: vectors::Point) {
+    pub fn set_endPoint(&mut self, point: Point) {
         self.endPoint = point;
     }
 }
 
 impl Object for Sphere {
-    fn intersection(&self, ray: vectors::VectorF) -> bool {
+    fn intersection(&self, ray: VectorF) -> bool {
         let result = resolve_quadratic_equation(ray.direction.x.powf(2.0) + ray.direction.y.powf(2.0) + ray.direction.z.powf(2.0),
                                    2.0 * (ray.direction.x * (ray.origin.x - self.origin.x) + ray.direction.y * (ray.origin.y - self.origin.y) + ray.direction.z * (ray.origin.z - self.origin.z)),
                                    ((ray.origin.x - self.origin.x).powf(2.0) + (ray.origin.y - self.origin.y).powf(2.0) + (ray.origin.z - self.origin.z).powf(2.0)) - self.radius.powf(2.0));
@@ -57,7 +59,7 @@ impl Object for Sphere {
 }
 
 impl Object for Plan {
-    fn intersection(&self, ray: vectors::VectorF) -> bool {
+    fn intersection(&self, ray: VectorF) -> bool {
         return true;
     }
 }
