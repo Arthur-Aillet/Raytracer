79f26b96821169c1fe4864306b9b93e77018d6c1
Auguste Frater <augustefrater@gmail.com>
2023-04-20 11:36:28 +0200

    feat: fully working json reader

--- src/json_parser/mod.rs
+++ src/json_parser/mod.rs
@@ -6,73 +6,76 @@
 //
 
 use std::fs;
-use serde_json::{Result, Value};
+use serde_json::Value;
 
-use crate::renderer::primitives::Sphere;
-
+#[derive(Debug, Clone, Copy)]
 pub struct Point {
-    x: f64,
-    y: f64,
-    z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct Transform {
-    pos: Point,
-    rotation: Point,
-    scale: Point,
+    pub pos: Point,
+    pub rotation: Point,
+    pub scale: Point,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct Camera {
-    transform: Transform,
-    focal_lenght: f64,
-    height: f64,
-    width: f64,
-    fov: f64,
+    pub transform: Transform,
+    pub focal_lenght: f64,
+    pub height: f64,
+    pub width: f64,
+    pub fov: f64,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct Color {
-    r: f64,
-    g: f64,
-    b: f64,
+    pub r: f64,
+    pub g: f64,
+    pub b: f64,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct Texture {
-    texture_type: String,
-    color: Color,
+    pub texture_type: u64,
+    pub color: Color,
 }
 
 impl Texture {
-    fn texture(&self, x: f64, y: f64) -> Color {
-        if self.texture_type == "color" {
+    fn texture(&self, _x: f64, _y: f64) -> Color {
+        if self.texture_type == 1 {
             self.color
         } else {self.color}
     }
 }
 
 pub struct Sphere {
-    transform: Transform,
-    texture: Texture,
-    radius: f64,
+    pub transform: Transform,
+    pub texture: Texture,
+    pub radius: f64,
 }
 
 pub struct Plane {
-    transform: Transform,
-    texture: Texture,
-    origin: Point,
-    vector: Point,
+    pub transform: Transform,
+    pub texture: Texture,
+    pub origin: Point,
+    pub vector: Point,
 }
 pub struct Cylinder {
-    transform: Transform,
-    texture: Texture,
-    height: f64,
-    radius: f64,
+    pub transform: Transform,
+    pub texture: Texture,
+    pub height: f64,
+    pub radius: f64,
 }
 
 pub struct Cone {
-    transform: Transform,
-    texture: Texture,
-    radius: f64,
-    height: f64,
+    pub transform: Transform,
+    pub texture: Texture,
+    pub radius: f64,
+    pub height: f64,
 }
 
 trait Object {
@@ -112,17 +115,17 @@ impl Object for Sphere {
     fn volume(&self) -> f64 {((std::f64::consts::PI * self.radius.powi(3)) * 4.0) / 3.0}
 
     fn get_height(&self) -> f64 {1.0}
-    fn set_height(&mut self, new: f64) {}
+    fn set_height(&mut self, _new: f64) {}
     fn get_origin(&self) -> Point {
         let a = Point{x: 0.0, y: 0.0, z: 0.0};
         a
     }
-    fn set_origin(&mut self, new: Point) {}
+    fn set_origin(&mut self, _new: Point) {}
     fn get_vector(&self) -> Point {
         let a = Point{x: 0.0, y: 0.0, z: 0.0};
         a
     }
-    fn set_vector(&mut self, new: Point) {}
+    fn set_vector(&mut self, _new: Point) {}
     fn slanted_height(&self) -> f64 {1.0}
     fn lateral_surface(&self) -> f64 {self.surface()}
     fn base_surface(&self) -> f64 {self.surface()}
@@ -140,9 +143,9 @@ impl Object for Plane {
     fn set_vector(&mut self, new: Point) {self.vector = new}
 
     fn get_radius(&self) -> f64 {1.0}
-    fn set_radius(&mut self, new: f64) {}
+    fn set_radius(&mut self, _new: f64) {}
     fn get_height(&self) -> f64 {1.0}
-    fn set_height(&mut self, new: f64) {}
+    fn set_height(&mut self, _new: f64) {}
     fn slanted_height(&self) -> f64 {1.0}
     fn diameter(&self) -> f64 {1.0}
     fn perimeter(&self) -> f64 {1.0}
@@ -173,12 +176,12 @@ impl Object for Cylinder {
         let a = Point{x: 0.0, y: 0.0, z: 0.0};
         a
     }
-    fn set_origin(&mut self, new: Point) {}
+    fn set_origin(&mut self, _new: Point) {}
     fn get_vector(&self) -> Point {
         let a = Point{x: 0.0, y: 0.0, z: 0.0};
         a
     }
-    fn set_vector(&mut self, new: Point) {}
+    fn set_vector(&mut self, _new: Point) {}
     fn slanted_height(&self) -> f64 {1.0}
 }
 
@@ -204,17 +207,17 @@ impl Object for Cone {
         let a = Point{x: 0.0, y: 0.0, z: 0.0};
         a
     }
-    fn set_origin(&mut self, new: Point) {}
+    fn set_origin(&mut self, _new: Point) {}
     fn get_vector(&self) -> Point {
         let a = Point{x: 0.0, y: 0.0, z: 0.0};
         a
     }
-    fn set_vector(&mut self, new: Point) {}
+    fn set_vector(&mut self, _new: Point) {}
 }
 pub struct Directional {
-    transform: Transform,
-    color: Color,
-    strength: f64,
+    pub transform: Transform,
+    pub color: Color,
+    pub strength: f64,
 }
 
 trait Light {
@@ -224,7 +227,7 @@ trait Light {
     fn get_color(&self) -> Color;
     fn set_color(&mut self, new: Color);
     fn get_strength(&self) -> f64;
-    fn set_strength(&self, new: f64);
+    fn set_strength(&mut self, new: f64);
 }
 
 impl Light for Directional {
@@ -234,28 +237,62 @@ impl Light for Directional {
     fn get_color(&self) -> Color {self.color}
     fn set_color(&mut self, new: Color) {self.color = new}
     fn get_strength(&self) -> f64 {self.strength}
-    fn set_strength(&self, new: f64) {self.strength = new}
+    fn set_strength(&mut self, new: f64) {self.strength = new}
 }
 
 pub struct Ambiant {
-    color: Color,
-    strength: f64,
+    pub color: Color,
+    pub strength: f64,
 }
 
 pub struct Lights {
-    lights: Vec::<Box::<dyn Light>>,
-    ambiant: Vec<Ambiant>,
+    pub lights: Vec::<Box::<dyn Light>>,
+    pub ambiant: Vec<Ambiant>,
 }
 
 pub struct Renderer {
-    camera: Camera,
-    primitives: Vec::<Box::<dyn Object>>,
-    lights: Lights,
+    pub camera: Camera,
+    pub primitives: Vec::<Box::<dyn Object>>,
+    pub lights: Lights,
 }
 
 impl Renderer {
 
-    pub fn get_point_from_json(&self, json: Value) -> Point{
+    pub fn new() -> Renderer {
+        let renderer = Renderer {
+            camera: Camera {
+                transform: Transform {
+                    pos: Point {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 0.0
+                    },
+                    rotation: Point {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 0.0
+                    },
+                    scale: Point {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 0.0
+                    }
+                },
+                focal_lenght: 0.0,
+                height: 0.0,
+                width: 0.0,
+                fov: 0.0
+            },
+            primitives: Vec::new(),
+            lights: Lights {
+                lights: Vec::new(),
+                ambiant: Vec::new(),
+            },
+        };
+        renderer
+    }
+
+    fn get_point_from_json(&self, json: &Value) -> Point{
         let point = Point {
             x: json["x"].as_f64().unwrap(),
             y: json["y"].as_f64().unwrap(),
@@ -264,18 +301,18 @@ impl Renderer {
         point
     }
 
-    pub fn get_transform_from_json(&self, json: Value) -> Transform {
-        let Transform = Transform {
-            pos: self.get_point_from_json(json["pos"]),
-            rotation: self.get_point_from_json(json["rotation"]),
-            scale: self.get_point_from_json(json["scale"])
+    fn get_transform_from_json(&self, json: &Value) -> Transform {
+        let transform = Transform {
+            pos: self.get_point_from_json(&json["pos"]),
+            rotation: self.get_point_from_json(&json["rotation"]),
+            scale: self.get_point_from_json(&json["scale"])
         };
-        Transform
+        transform
     }
 
-    fn get_camera_from_json(&self, json: Value) -> Camera {
+    fn get_camera_from_json(&self, json: &Value) -> Camera {
         let camera = Camera {
-            transform: self.get_transform_from_json(json["transform"]),
+            transform: self.get_transform_from_json(&json["transform"]),
             focal_lenght: json["focal_lenght"].as_f64().unwrap(),
             height: json["height"].as_f64().unwrap(),
             width: json["width"].as_f64().unwrap(),
@@ -284,7 +321,7 @@ impl Renderer {
         camera
     }
 
-    fn get_color_from_json(&self, json: Value) -> Color {
+    fn get_color_from_json(&self, json: &Value) -> Color {
         let color = Color {
             r: (json["r"].as_f64().unwrap() % 255.0) / 255.0,
             g: (json["g"].as_f64().unwrap() % 255.0) / 255.0,
@@ -293,39 +330,39 @@ impl Renderer {
         color
     }
 
-    fn get_texture_from_json(&self, json: Value) -> Texture {
+    fn get_texture_from_json(&self, json: &Value) -> Texture {
         let texture = Texture {
-            texture_type: json["texture_type"].as_str().unwrap().to_string(),
-            color: self.get_color_from_json(json["color"]),
+            texture_type: json["texture_type"].as_u64().unwrap(),
+            color: self.get_color_from_json(&json["color"]),
         };
         texture
     }
 
-    fn get_sphere_from_json(&self, json: Value) -> Box::<Sphere> {
+    fn get_sphere_from_json(&self, json: &Value) -> Box::<Sphere> {
         let sphere = Sphere {
-            transform: self.get_transform_from_json(json["transform"]),
-            texture: self.get_texture_from_json(json["transform"]),
+            transform: self.get_transform_from_json(&json["transform"]),
+            texture: self.get_texture_from_json(&json["texture"]),
             radius: json["radius"].as_f64().unwrap(),
         };
         let spherebox = Box::new(sphere);
         spherebox
     }
 
-    fn get_plane_from_json(&self, json: Value) -> Box::<Plane> {
+    fn get_plane_from_json(&self, json: &Value) -> Box::<Plane> {
         let plane = Plane {
-            transform: self.get_transform_from_json(json["transform"]),
-            texture: self.get_texture_from_json(json["transform"]),
-            origin: self.get_point_from_json(json["origin"]),
-            vector: self.get_point_from_json(json["vector"]),
+            transform: self.get_transform_from_json(&json["transform"]),
+            texture: self.get_texture_from_json(&json["texture"]),
+            origin: self.get_point_from_json(&json["origin"]),
+            vector: self.get_point_from_json(&json["vector"]),
         };
         let planebox = Box::new(plane);
         planebox
     }
 
-    fn get_cylinder_from_json(&self, json: Value) -> Box::<Cylinder> {
+    fn get_cylinder_from_json(&self, json: &Value) -> Box::<Cylinder> {
         let cylinder = Cylinder {
-            transform: self.get_transform_from_json(json["transform"]),
-            texture: self.get_texture_from_json(json["transform"]),
+            transform: self.get_transform_from_json(&json["transform"]),
+            texture: self.get_texture_from_json(&json["texture"]),
             height: json["height"].as_f64().unwrap(),
             radius: json["radius"].as_f64().unwrap(),
         };
@@ -333,10 +370,10 @@ impl Renderer {
         cylinderbox
     }
 
-    fn get_cone_from_json(&self, json: Value) -> Box::<Cone> {
+    fn get_cone_from_json(&self, json: &Value) -> Box::<Cone> {
         let cone = Cone {
-            transform: self.get_transform_from_json(json["transform"]),
-            texture: self.get_texture_from_json(json["transform"]),
+            transform: self.get_transform_from_json(&json["transform"]),
+            texture: self.get_texture_from_json(&json["texture"]),
             height: json["height"].as_f64().unwrap(),
             radius: json["radius"].as_f64().unwrap(),
         };
@@ -344,73 +381,70 @@ impl Renderer {
         conebox
     }
 
-    fn get_objects_from_json(&self, json: Value) -> Vec::<Box::<dyn Object>> {
-        let objects: Vec::<Box::<dyn Object>>;
-        for sphere in json["spheres"].as_array().iter() {
+    fn get_objects_from_json(&self, json: &Value) -> Vec::<Box::<dyn Object>> {
+        let mut objects: Vec::<Box::<dyn Object>> = Vec::new();
+        for sphere in json["spheres"].as_array().unwrap().iter() {
             objects.push(self.get_sphere_from_json(sphere))
         }
-        for plane in json["planes"].as_array().iter() {
+        for plane in json["planes"].as_array().unwrap().iter() {
             objects.push(self.get_plane_from_json(plane))
         }
-        for cylinder in json["cylinders"].as_array().iter() {
+        for cylinder in json["cylinders"].as_array().unwrap().iter() {
             objects.push(self.get_cylinder_from_json(cylinder))
         }
-        for cone in json["cones"].as_array().iter() {
+        for cone in json["cones"].as_array().unwrap().iter() {
             objects.push(self.get_cone_from_json(cone))
         }
         objects
     }
 
-    fn get_directional_from_json(&self, json: Value) -> Box::<Directional> {
+    fn get_directional_from_json(&self, json: &Value) -> Box::<Directional> {
         let directional = Directional {
-            transform: self.get_transform_from_json(json["transform"]),
-            color: self.get_color_from_json(json["color"]),
+            transform: self.get_transform_from_json(&json["transform"]),
+            color: self.get_color_from_json(&json["color"]),
             strength: json["strength"].as_f64().unwrap(),
         };
         let directionalbox = Box::new(directional);
         directionalbox
     }
 
-    fn get_object_lights_from_json(&self, json: Value) -> Vec::<Box::<dyn Light>> {
-        let lights: Vec::<Box::<dyn Light>>;
-        for directional in json["directional"].as_array() {
+    fn get_object_lights_from_json(&self, json: &Value) -> Vec::<Box::<dyn Light>> {
+        let mut lights: Vec::<Box::<dyn Light>> = Vec::new();
+        for directional in json["directional"].as_array().unwrap().iter() {
             lights.push(self.get_directional_from_json(directional))
         }
         lights
     }
 
-    fn get_ambiant_from_json(&self, json: Value) -> Ambiant {
+    fn get_ambiant_from_json(&self, json: &Value) -> Ambiant {
         let ambiant = Ambiant {
-            color: self.get_color_from_json(json["color"]),
+            color: self.get_color_from_json(&json["color"]),
             strength: json["strength"].as_f64().unwrap(),
         };
         ambiant
     }
 
-    fn get_ambiants_from_json(&self, json: Value) -> Vec::<Ambiant> {
-        let lights: Vec::<Ambiant>;
-        for ambiant in json["ambiant"].as_array().iter() {
+    fn get_ambiants_from_json(&self, json: &Value) -> Vec::<Ambiant> {
+        let mut lights: Vec::<Ambiant> = Vec::new();
+        for ambiant in json["ambiant"].as_array().unwrap().iter() {
             lights.push(self.get_ambiant_from_json(ambiant))
         }
         lights
     }
 
-    fn get_lights_from_json(&self, json: Value) -> Lights {
+    fn get_lights_from_json(&self, json: &Value) -> Lights {
         let lights = Lights {
-            lights: self.get_object_lights_from_json(json["objects"]),
-            ambiant: self.get_ambiants_from_json(json["ambiant"]),
+            lights: self.get_object_lights_from_json(&json["objects"]),
+            ambiant: self.get_ambiants_from_json(&json),
         };
         lights
     }
 
-    pub fn get_renderer_from_file(&self, file: String) -> Renderer {
-        let data = fs::read_to_string("src/config_file/ex.json").expect("Unable to read file");
+    pub fn get_renderer_from_file(&mut self, file: String) {
+        let data = fs::read_to_string(file).expect("Unable to read file");
         let json: Value = serde_json::from_str(&data.to_string()).unwrap();
-        let renderer = Renderer {
-            camera: self.get_camera_from_json(json["camera"]),
-            primitives: self.get_objects_from_json(json["primitives"]),
-            lights: self.get_lights_from_json(json["lights"]),
-        };
-        renderer
+        self.camera = self.get_camera_from_json(&json["camera"]);
+        self.primitives = self.get_objects_from_json(&json["primitives"]);
+        self.lights = self.get_lights_from_json(&json["lights"]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,21 @@
 // main
 //
 
-// use json_parser::Renderer;
+use json_parser::Renderer;
 use std::fs;
 use serde_json::{Result, Value};
 
 
 mod ppm_interface;
-mod renderer;
 mod vectors;
 mod matrix;
-// mod json_parser;
+mod json_parser;
 
-// fn main() -> std::io::Result<()> {
-//     let mut file = std::fs::File::open("config_file/ex.cfg").unwrap();
-//     let mut contents = String::new();
-//     file.read_to_string(&mut contents).unwrap();
-//     print!("{}", contents);
 
-//     Ok(())
-// }
-
-// fn main() {
-    // let renderer : Renderer;
-    // renderer.get_renderer_from_file("src/jsom_parser/ex.json".to_string());
-    // print!("test:{}", renderer.camera.height)
-    // let data = fs::read_to_string("src/config_file/ex.json").expect("Unable to read file");
-    // let json: Value = serde_json::from_str(&data.to_string()).unwrap();
-    // for element in json["primitives"]["spheres"].as_array() {
-    //     print!("{}\n", element[0]);
-    // }
-    fn main() {
-        let data = fs::read_to_string("src/config_file/ex.json").expect("Unable to read file");
-        let v: Value = serde_json::from_str(&data.to_string()).unwrap();
-        println!("{}", v);
-    }
-// }
+fn main() {
+    let mut renderer : Renderer = Renderer::new();
+    renderer.get_renderer_from_file("src/json_parser/ex.json".to_string());
+    let data = fs::read_to_string("src/json_parser/ex.json").expect("Unable to read file");
+    let _json: Value = serde_json::from_str(&data.to_string()).unwrap();
+    print!("test:{}\n", renderer.camera.height);
+}
