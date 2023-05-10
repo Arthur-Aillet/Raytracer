6687d51246f9934640e28519120c473aac5e7af6
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-05-10 15:28:26 +0200

    feat: base to handle inputs with nannou

--- src/nannou/mod.rs
+++ src/nannou/mod.rs
@@ -8,18 +8,17 @@ use nannou::Frame;
 use nannou::App;
 
 use crate::renderer::Renderer;
-use crate::config;
+use crate::config::Config;
 use std::env;
 
-struct Model {
-    _window: window::Id,
-    config: config::Config,
-    renderer: Renderer,
+pub struct Model {
+    window: WindowId,
+    config: Config,
 }
 
 fn model(app: &App) -> Model {
     let args: Vec<String> = env::args().collect();
-    let mut config = config::Config::from_args(&args);
+    let mut config = Config::from_args(&args);
     let window = app
         .new_window()
         .title("Rustracer")
@@ -28,32 +27,48 @@ fn model(app: &App) -> Model {
         .build()
         .expect("Failed to build the window");
 
-    config.height = config.height / config.fast_mode;
-    config.width = config.width / config.fast_mode;
+    config.height = config.height / if config.fast_mode == 0 { 1 } else { config.fast_mode };
+    config.width = config.width / if config.fast_mode == 0 { 1 } else { config.fast_mode };
     Model {
-        _window: window,
-        config: config.clone(),
-        renderer: Renderer::get_renderer_from_file(&config),
+        window,
+        config,
     }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(app: &App, model: &Model, frame: Frame) {
-    let pixels = model.renderer.render(&model.config);
+fn handle_keys(app: &App) {
+    if app.keys.down.contains(&Key::Left) {
+        println!("Left");
+    }
+    if app.keys.down.contains(&Key::Right) {
+        println!("Right");
+    }
+    if app.keys.down.contains(&Key::Up) {
+        println!("Up");
+    }
+    if app.keys.down.contains(&Key::Down) {
+        println!("Down");
+    }
+}
+
+fn view(_app: &App, model: &Model, frame: Frame) {
+    handle_keys(&_app);
+    let renderer = Renderer::get_renderer_from_file(&model.config);
+    let pixels = renderer.render(&model.config);
     let mut index = 0;
 
-    let window = app.window_rect();
+    let window = _app.window_rect();
     let view = window.pad(100.0);
 
-    let draw = app.draw();
+    let draw = _app.draw();
     draw.background().color(BLUE);
     for y in (-model.config.height / 2)..(model.config.height / 2) {
         for x in (-model.config.width / 2)..(model.config.width / 2) {
             let color = pixels[index..index + 3].to_vec();
             draw.rect()
-                .x_y((x as f32) * model.config.fast_mode as f32, (-y as f32) * model.config.fast_mode as f32)
-                .w_h(model.config.fast_mode as f32, model.config.fast_mode as f32)
+                .x_y((x as f32) * (if model.config.fast_mode == 0 { 1 } else { model.config.fast_mode })as f32, (-y as f32) * (if model.config.fast_mode == 0 { 1 } else { model.config.fast_mode }) as f32)
+                .w_h((if model.config.fast_mode == 0 { 1 } else { model.config.fast_mode }) as f32, (if model.config.fast_mode == 0 { 1 } else { model.config.fast_mode }) as f32)
                 .color(Rgb::new(
                     color[0] as f32 / 255.0,
                     color[1] as f32 / 255.0,
@@ -62,7 +77,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
             index += 3;
         }
     }
-    draw.to_frame(app, &frame).unwrap();
+    draw.to_frame(_app, &frame).unwrap();
 }
 
 pub struct NannouInterface {
