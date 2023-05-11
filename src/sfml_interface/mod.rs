9aa783aebff5fdba683364156b7a5b498e418f73
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-05-12 01:38:51 +0200

    feat: update graphic mode bool -> i64, in order to choose graphical mode + update help

--- src/sfml_interface/mod.rs
+++ src/sfml_interface/mod.rs
@@ -14,4 +14,37 @@ use crate::config::Config;
 
 pub struct SfmlInterface {
     config: Config,
+    window: RenderWindow
+}
+
+impl SfmlInterface {
+    pub fn new(mut config: &Config) -> Self {
+        let mut window = RenderWindow::new(
+            (config.width, config.height),
+            "Rustracer",
+            Style::CLOSE,
+            &Default::default(),
+        );
+        window.set_vertical_sync_enabled(true);
+        window.set_framerate_limit(60);
+
+
+        SfmlInterface {
+            config,
+            window
+        }
+    }
+
+    pub fn run(&mut self) {
+        while self.window.is_open() {
+            while let Some(event) = self.window.poll_event() {
+                match event {
+                    Event::Closed => self.window.close(),
+                    _ => {}
+                }
+            }
+            self.window.clear(&Color::BLACK);
+            self.window.display();
+        }
+    }
 }
