568eac1c2908d714d096e8674923ad91ed2ff2f1
Aillet <arthur.aillet@epitech.eu>
2023-05-12 03:09:19 +0200

    feat: fast triangle object created, render time 20 times faster

--- src/renderer/primitives/mesh.rs
+++ src/renderer/primitives/mesh.rs
@@ -14,14 +14,74 @@ use serde::{Deserialize, Serialize};
 use erased_serde::serialize_trait_object;
 
 #[derive(Deserialize, Serialize)]
-pub struct Mesh {
+pub struct FastTriangle {
+    pub point_a: Vector,
+    pub point_b: Vector,
+    pub point_c: Vector,
+    pub texture: Texture,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Mesh  {
     pub transform: Transform,
     pub texture: Texture,
-    pub triangles: Vec<Triangle>,
+    pub triangles: Vec<FastTriangle>,
+}
+
+impl Object for FastTriangle {
+    fn intersection(&self, ray: Vector, origin: Vector) -> Option<Intersection> {
+        let mut normal = (self.point_b - self.point_a).cross_product(self.point_c - self.point_a).normalize();
+
+        let denom = ray.normalize().dot_product(normal);
+        if denom == 0.0 {
+            return None
+        }
+        let progress = (((self.point_a + self.point_b + self.point_c) / 3.0) - origin).dot_product(normal) / denom;
+        if progress < 0.0 {
+            return None
+        }
+        let intersection_point = Vector{
+            x: origin.x + ray.x * progress,
+            y: origin.y + ray.y * progress,
+            z: origin.z + ray.z * progress
+        };
+
+        let cross = (self.point_b - self.point_a).cross_product(intersection_point - self.point_a);
+        if normal.dot_product(cross) < 0.0 {
+            return None;
+        }
+
+        let cross = (self.point_c - self.point_b).cross_product(intersection_point - self.point_b);
+        if normal.dot_product(cross) < 0.0 {
+            return None;
+        }
+
+        let cross = (self.point_a - self.point_c).cross_product(intersection_point - self.point_c);
+        if normal.dot_product(cross) < 0.0 {
+            return None;
+        }
+
+        if normal.dot_product(origin - intersection_point) < 0.0 {
+            normal = normal * -1.0;
+        }
+
+        Some ( Intersection {
+            intersection_point,
+            normal,
+            object: Some(self),
+            light: None,
+        })
+    }
+    fn surface_position(&self, position: Vector) -> Vector {Vector { x: 0.5, y: 0.5, z: 0.0}}
+    fn get_transform(&self) -> Transform {Transform::default()}
+    fn move_obj(&mut self, _offset: Transform) {;}
+    fn set_transform(&mut self, _new: Transform) {}
+    fn get_texture(&self) -> Texture {self.texture.clone()}
+    fn set_texture(&mut self, _new: Texture) {}
 }
 
 impl Mesh {
-    pub fn parse_face(&mut self, line :String, verteces : &Vec<Vector>) -> (Option<Triangle>, Option<Triangle>){
+    pub fn parse_face(&mut self, line :String, verteces : &Vec<Vector>) -> (Option<FastTriangle>, Option<FastTriangle>){
         let points: Vec<&str> = line.split_ascii_whitespace()
                                     .filter(|&x| !x.is_empty())
                                     .skip(1)
@@ -57,22 +117,25 @@ impl Mesh {
             }
             count += 1;
         }
-        let fst_triangle: Triangle = Triangle {
-            transform: self.transform,
+
+        points_res[0].rotate(self.transform.rotation.x, self.transform.rotation.y, self.transform.rotation.z);
+        points_res[1].rotate(self.transform.rotation.x, self.transform.rotation.y, self.transform.rotation.z);
+        points_res[2].rotate(self.transform.rotation.x, self.transform.rotation.y, self.transform.rotation.z);
+        let fst_triangle: FastTriangle = FastTriangle {
+            point_a: points_res[0] + self.transform.pos,
+            point_b: points_res[1] + self.transform.pos,
+            point_c: points_res[2] + self.transform.pos,
             texture: self.texture.clone(),
-            point_a: points_res[0],
-            point_b: points_res[1],
-            point_c: points_res[2],
         };
         if len == 3 {
             return (Some(fst_triangle), None);
         }
-        let snd_triangle: Triangle = Triangle {
-            transform: self.transform,
+        points_res[3].rotate(self.transform.rotation.x, self.transform.rotation.y, self.transform.rotation.z);
+        let snd_triangle: FastTriangle = FastTriangle {
+            point_a: points_res[2] + self.transform.pos,
+            point_b: points_res[3] + self.transform.pos,
+            point_c: points_res[0] + self.transform.pos,
             texture: self.texture.clone(),
-            point_a: points_res[2],
-            point_b: points_res[3],
-            point_c: points_res[0],
         };
         return (Some(fst_triangle), Some(snd_triangle));
     }
