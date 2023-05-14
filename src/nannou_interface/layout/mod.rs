a9c330ee3a251549ef1df920a37d8da2fc44cc77
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-05-14 13:37:08 +0200

    refactor: switch display from layout to components theirselves

--- src/nannou_interface/layout/mod.rs
+++ src/nannou_interface/layout/mod.rs
@@ -42,7 +42,6 @@ impl Layout {
 
         let mut inputs = Vec::new();
         let mut checkboxes = Vec::new();
-        let mut texts = Vec::new();
         let mut image = Vec::new();
 
         let mut buttons = vec![
@@ -51,7 +50,10 @@ impl Layout {
             Button::new("exit".to_string(), width - 180.0, -height + 50.0, 280.0, 50.0, String::from("EXIT")),
         ];
         let mut sliders = vec![
-            Slider::new("fov".to_string(), width - 180.0, height - 120.0, 280.0, 50.0, String::from("FOV"), renderer.camera.fov, 0, 180),
+            Slider::new("fov".to_string(), width - 180.0, height - 120.0, 280.0, 50.0, String::from("FOV : "), renderer.camera.fov, 0, 180),
+        ];
+        let mut texts = vec![
+            Text::new("object info".to_string(), width - 180.0, height - 120.0, 280.0, 50.0, String::from("Obj infos : ")),
         ];
 
         Layout {
@@ -67,89 +69,13 @@ impl Layout {
         }
     }
 
-    fn display_buttons(&mut self, app: &App, draw: &Draw) {
-        let mouse_position = app.mouse.position();
-
-        for button in &mut self.buttons {
-            if button.rect.contains(mouse_position) {
-                if app.mouse.buttons.left().is_down() {
-                    draw.rect()
-                        .x_y(button.rect.x(), button.rect.y())
-                        .w_h(button.rect.w(), button.rect.h())
-                        .color(nannou::color::IntoLinSrgba::<f32>::into_lin_srgba(nannou::color::rgb_u32(0xA68F6D)));
-                } else {
-                    draw.rect()
-                        .x_y(button.rect.x(), button.rect.y())
-                        .w_h(button.rect.w(), button.rect.h())
-                        .color(nannou::color::IntoLinSrgba::<f32>::into_lin_srgba(nannou::color::rgb_u32(0xA6701E)));
-                }
-            } else {
-                draw.rect()
-                    .x_y(button.rect.x(), button.rect.y())
-                    .w_h(button.rect.w(), button.rect.h())
-                    .color(nannou::color::IntoLinSrgba::<f32>::into_lin_srgba(nannou::color::rgb_u32(0xEB9E2C)));
-            }
-            draw.text(&button.text)
-                .x_y(button.rect.x(), button.rect.y())
-                .w_h(button.rect.w(), button.rect.h())
-                .color(nannou::color::IntoLinSrgba::<f32>::into_lin_srgba(nannou::color::rgb_u32(0xFFFFFF)));
-        }
-    }
-
-    pub fn display_sliders(&mut self, app: &App, draw: &Draw) {
-        let mouse_position = app.mouse.position();
-
-        for slider in &mut self.sliders {
-            let percent = (slider.value - slider.min) as f32 / (slider.max - slider.min) as f32;
-            let mut cursor_rect = Rect::from_x_y_w_h(
-                slider.rect.x() - (slider.rect.w() / 2.0) + (slider.rect.w() * percent as f32),
-                slider.rect.y(),
-                slider.rect.h() / 1.8,
-                slider.rect.h() / 1.8,
-            );
-
-            if cursor_rect.contains(mouse_position) {
-                if app.mouse.buttons.left().is_down() {
-                    cursor_rect.x.start = mouse_position.x - (cursor_rect.w() / 2.0);
-                    cursor_rect.x.end = mouse_position.x + (cursor_rect.w() / 2.0);
-                }
-                // limit cursor to slider
-                if cursor_rect.x.start < slider.rect.x() - (slider.rect.w() / 2.0) {
-                    cursor_rect.x.start = slider.rect.x() - (slider.rect.w() / 2.0);
-                    cursor_rect.x.end = cursor_rect.x.start + cursor_rect.w();
-                } else if cursor_rect.x.end > slider.rect.x() + (slider.rect.w() / 2.0) {
-                    cursor_rect.x.end = slider.rect.x() + (slider.rect.w() / 2.0);
-                    cursor_rect.x.start = cursor_rect.x.end - cursor_rect.w();
-                }
-            }
-
-            //draw background
-            draw.rect()
-                .x_y(slider.rect.x(), slider.rect.y())
-                .w_h(slider.rect.w(), slider.rect.h() / 2.2)
-                .color(nannou::color::IntoLinSrgba::<f32>::into_lin_srgba(nannou::color::rgb_u32(0x3F3944)));
-            //draw curent value bar
-            draw.rect()
-                .x_y((slider.rect.x() - (slider.rect.w() / 2.0)) + ((slider.rect.w() * percent as f32) / 2.0), slider.rect.y())
-                .w_h(slider.rect.w() * percent, slider.rect.h() / 2.2)
-                .color(nannou::color::IntoLinSrgba::<f32>::into_lin_srgba(nannou::color::rgb_u32(0xA6701E)));
-            //draw cursor
-            draw.rect()
-                .x_y(cursor_rect.x(), cursor_rect.y())
-                .w_h(cursor_rect.w(), cursor_rect.h())
-                .color(nannou::color::IntoLinSrgba::<f32>::into_lin_srgba(nannou::color::rgb_u32(0xEB9E2C)));
-
-            draw.text(&slider.text)
-                .x_y(slider.rect.x(), slider.rect.y() + 25.0)
-                .w_h(slider.rect.w(), slider.rect.h())
-                .color(nannou::color::IntoLinSrgba::<f32>::into_lin_srgba(nannou::color::rgb_u32(0xFFFFFF)));
-            slider.value = (((cursor_rect.x() - (slider.rect.x() - (slider.rect.w() / 2.0))) as f32 / slider.rect.w()) as f32 * (slider.max - slider.min) as f32 + slider.min as f32) as i64;
-        }
-    }
-
     pub fn display(&mut self, app: &App, draw: &Draw) {
-        self.display_buttons(app, draw);
-        self.display_sliders(app, draw);
+        for button in &mut self.buttons {
+            button.display(app, draw);
+        }
+        for slider in &mut self.sliders {
+            slider.display(app, draw);
+        }
     }
 
     pub fn get_buttons_interactions(&self, app: &App, name: String) -> bool {
